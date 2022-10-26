3234349fd97d4fad6dde5e77abce0b23dd0c855e
Shaina <97140374+borahae13@users.noreply.github.com>
2022-10-26 13:39:42 -0400

    Edited GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <Weather />
-      <footer>
-        Open source{" "}
-        <a
-          href="https://github.com/borahae13/react-weather-app"
-          target="_blank"
-        >
-          code
-        </a>{" "}
-        by Shaina C.
-      </footer>
+      <div className="container">
+        <h1>Hello World!</h1>
+        <Weather />
+        <footer>
+          Open source{" "}
+          <a
+            href="https://github.com/borahae13/react-weather-app"
+            target="_blank"
+          >
+            code
+          </a>{" "}
+          by Shaina C.
+        </footer>
+      </div>
     </div>
   );
 }
