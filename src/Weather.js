e7b64779ddd461d3e6670d4c48c8a30e3e95b8ac
Shaina <97140374+borahae13@users.noreply.github.com>
2022-10-26 16:51:09 -0400

    Edited Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,7 @@
 import React from "react";
 import axios from "axios";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Weather() {
-  function handleResponse(response) {
-    alert(`The weather in New York is ${response.data.main.temp}C`);
-  }
-  let city = "New York";
-  let apiKey = "e4a579ffa1b19f3e711a89d8fabe3010";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(handleResponse);
   return <h2>Hello from weather</h2>;
 }
