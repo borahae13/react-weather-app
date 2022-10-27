cfd4f571fbf0bd5d01eb7af195380d9979bdc303
Shaina <97140374+borahae13@users.noreply.github.com>
2022-10-27 14:19:55 -0400

    Updated Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,4 @@
 import React from "react";
-import axios from "axios";
-import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Weather() {
   return <h2>Hello from weather</h2>;
